ad2f842596b01f6a1897b575cfea4863364f5e9f
normanBPT <norman@axion.ltd>
2021-10-26 11:44:04 +0700

    modif 20211026

--- configure.sh
+++ configure.sh
@@ -3,8 +3,10 @@ echo please enter pg username
 read pguser
 echo please enter pg password for $pguser
 read pgpass
-echo username is $pguser and password is $pgpass
+echo please enter database name
+read dbname
+echo initiate database, please wait...
 export PGPASSWORD=$pgpass
-dropdb -U $pguser --if-exists jubelio
-createdb -U $pguser jubelio
-
+dropdb -U $pguser --if-exists $dbname || echo "somethings wrong with username & password"
+createdb -U $pguser $dbname
+echo initiation finish
